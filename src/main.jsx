1cf620e18d00fb4fad4765853486d9b7db12153f
arifnayan2861 <am.nayan2861@gmail.com>
2024-05-09 22:24:57 +0600

    navbar added

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
 import "./index.css";
 import routes from "./routes/Routes";
+import AuthProvider from "./context/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={routes} />
+    <AuthProvider>
+      <RouterProvider router={routes} />
+      <Toaster />
+    </AuthProvider>
   </React.StrictMode>
 );
