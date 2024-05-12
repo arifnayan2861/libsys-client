0e63810299c009ceb01906b4a038b5d2b1bd0ba3
arifnayan2861 <am.nayan2861@gmail.com>
2024-05-12 20:24:01 +0600

    update book page updated

--- src/pages/UpdateBook/UpdateBook.jsx
+++ src/pages/UpdateBook/UpdateBook.jsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
 import { useNavigate, useParams } from "react-router-dom";
 
@@ -8,11 +7,6 @@ const UpdateBook = () => {
   const navigate = useNavigate();
   const { id } = useParams();
   const [book, setBook] = useState({});
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
 
   const getData = async () => {
     const { data } = await axios.get(
@@ -24,47 +18,43 @@ const UpdateBook = () => {
   useEffect(() => {
     getData();
   }, []);
-  console.log(book);
 
-  const handleUpdateBook = (data) => {
+  const handleUpdateBook = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const img = form.imgUrl.value;
+    const bookName = form.bookName.value;
+    const authorName = form.authorName.value;
+    const category = form.category.value;
+    const rating = form.rating.value;
+    // console.log(bookName, category);
+    const updatedBook = {
+      img,
+      bookName,
+      authorName,
+      category,
+      rating,
+    };
     try {
-      axios.put(`${import.meta.env.VITE_BACKEND_URL}/update-book/${id}`, {
-        img: data.imgUrl,
-        bookName: data.bookName,
-        authorName: data.authorName,
-        category: data.category,
-        rating: data.rating,
-      });
+      axios.put(
+        `${import.meta.env.VITE_BACKEND_URL}/update-book/${id}`,
+        updatedBook
+      );
       toast.success("Book info updated successfully!");
       navigate("/all-books");
     } catch (error) {
       console.log(error.message);
       toast.success(error.message);
     }
-    // axios
-    //   .put(`${import.meta.env.VITE_BACKEND_URL}/update-book/${id}`, {
-    //       img: data.imgUrl,
-    //     bookName: data.bookName,
-    //     authorName: data.authorName,
-    //     category: data.category,
-    //     rating: data.rating,
-    //   })
-    //   .then((res) => {
-    //     console.log(res.data);
-    //     toast.success("Book added successfully!");
-    //   })
-    //   .catch((error) => console.log(error.message));
+    console.log(category);
   };
 
   return (
     <div className="w-[80%] max-w-sm mx-auto mt-14">
-      <form
-        className="max-w-md mx-auto"
-        onSubmit={handleSubmit(handleUpdateBook)}
-      >
+      <form className="max-w-md mx-auto" onSubmit={handleUpdateBook}>
         <div className="relative z-0 w-full mb-5 group">
           <input
-            {...register("bookName", { required: true })}
+            name="bookName"
             type="text"
             id="bookName"
             placeholder=""
@@ -78,15 +68,10 @@ const UpdateBook = () => {
           >
             Book name
           </label>
-          {errors.bookName && (
-            <span className="text-red-500 italic text-sm">
-              This field is required
-            </span>
-          )}
         </div>
         <div className="relative z-0 w-full mb-5 group">
           <input
-            {...register("imgUrl", { required: true })}
+            name="imgUrl"
             type="text"
             id="imgUrl"
             placeholder=""
@@ -99,16 +84,11 @@ const UpdateBook = () => {
           >
             Book image
           </label>
-          {errors.imgUrl && (
-            <span className="text-red-500 italic text-sm">
-              This field is required
-            </span>
-          )}
         </div>
         <div className="grid md:grid-cols-2 md:gap-6">
           <div className="relative z-0 w-full mb-5 group">
             <input
-              {...register("authorName", { required: true })}
+              name="authorName"
               type="text"
               id="authorName"
               className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
@@ -121,37 +101,30 @@ const UpdateBook = () => {
             >
               Author name
             </label>
-            {errors.authorName && (
-              <span className="text-red-500 italic text-sm">
-                This field is required
-              </span>
-            )}
           </div>
           <div className="relative z-0 w-full mb-5 group">
-            <input
-              {...register("category", { required: true })}
-              type="text"
+            <select
+              name="category"
               id="category"
-              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-              placeholder=""
               defaultValue={book.category}
-            />
+              className="w-full rounded-md border border-gray-300 py-1 px-3 mt-3 text-gray-700 focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 shadow-sm dark:border-gray-600 dark:text-gray-400"
+            >
+              <option value="Non-fiction">Non-fiction</option>
+              <option value="Drama">Drama</option>
+              <option value="Sci-fi">Sci-fi</option>
+              <option value="Thriller">Thriller</option>
+            </select>
             <label
               htmlFor="category"
-              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              className="text-[0.7rem] absolute -top-2 text-gray-700 dark:text-gray-400 block mb-2"
             >
               Category
             </label>
-            {errors.category && (
-              <span className="text-red-500 italic text-sm">
-                This field is required
-              </span>
-            )}
           </div>
         </div>
         <div className="relative z-0 w-full mb-5 group">
           <input
-            {...register("rating", { required: true })}
+            name="rating"
             type="text"
             id="rating"
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
@@ -164,11 +137,6 @@ const UpdateBook = () => {
           >
             Rating
           </label>
-          {errors.rating && (
-            <span className="text-red-500 italic text-sm">
-              This field is required
-            </span>
-          )}
         </div>
         <div className="text-end">
           <button
